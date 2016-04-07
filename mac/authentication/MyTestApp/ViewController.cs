5ea8989bf1336b8c5989e38b453b50267527b4ed
Tyler Long <tyler4long@gmail.com>
2016-04-07 13:02:18 +0800

    Add code of authentication

---
+++ mac/authentication/MyTestApp/ViewController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+using RingCentral.SDK;
+
+using UIKit;
+
+namespace MyTestApp
+{
+	public partial class ViewController : UIViewController
+	{
+		public ViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		private Platform platform;
+		private const string appKey = "appKey";
+		private const string appSecret = "appSecret";
+		private const string username = "username";
+		private const string extension = "extension";
+		private const string password = "password";
+		private const string sandboxServer = "https://platform.devtest.ringcentral.com";
+		// private const string productionServer = "https://platform.ringcentral.com";
+
+		private void Authenticate ()
+		{
+			if (platform == null) {
+				var sdk = new SDK (appKey, appSecret, sandboxServer, "MyTestApp", "1.0.0");
+				platform = sdk.GetPlatform ();
+			}
+			if (!platform.IsAuthorized ()) {
+				platform.Authorize (username, extension, password, true);
+			}
+		}
+
+		private void PrintAuthenticationStatus ()
+		{
+			if (platform != null && platform.IsAuthorized ()) {
+				Console.WriteLine ("App is authenticated");
+			} else {
+				Console.WriteLine ("App is not authenticated");
+			}
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			// Perform any additional setup after loading the view, typically from a nib.
+
+			// do the authentication
+			Authenticate ();
+
+			// make sure authentication is successful
+			PrintAuthenticationStatus ();
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
